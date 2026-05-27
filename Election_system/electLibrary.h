6bb090a41bb7c545e4755b2941a0989e4c297f07
Alan Wani <alan132301@gmail.com>
2026-05-27 11:27:40 +0300

    Removed structures.h and moved its content to electLibrary.h, also updated main.c to reflect the changes.

--- Election_system/electLibrary.h
+++ Election_system/electLibrary.h
@@ -1,3 +1,10 @@
+#ifndef ELECTLIBRARY_H
+#define ELECTLIBRARY_H
+
+#define MAX_VOTERS 100
+#define MAX_CANDIDATES 10
+#define NAME_LEN 100
+
 int isValidPosition(char* pos);
 struct Voter* findVoter(int id);
 struct Candidate* findCandidate(int id);
@@ -11,3 +18,4 @@ void castVote();
 void displayResults();
 
 
+#endif // ELECTLIBRARY_H
--- Election_system/main.c
+++ Election_system/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "electLibrary.h"
 
 struct Voter{
 int id;
@@ -16,6 +17,36 @@ bool hasVoted;
 
 int main()
 {
-    printf("Hello world!\n");
+int choice = 0;
+
+switch(choice){
+case 0:
+    addPosition();
+    break;
+case 1:
+    registerVoter();
+    break;
+case 2:
+    registerCandidate();
+    break;
+case 3:
+    searchRecord();
+    break;
+case 4:
+    updateVoter();
+    break;
+case 5:
+    deleteVoter();
+    break;
+case 6:
+    castVote();
+    break;
+case 7:
+    displayResults();
+    break;        
+default:
+    printf("Invalid selection\n");
+
+}
     return 0;
 }
